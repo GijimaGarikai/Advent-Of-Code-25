4026290c1accff633ae811ea3f04e52f88db19bd
Garikai <gijimaga@grinnell.edu>
2025-12-01 14:44:44 -0600

    Day 1

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -19,14 +19,16 @@ for line in lines:
     direction = 1 if line[0] == 'R' else -1
     turns = int(line[1:])
     interimVal = cur + (direction*turns)
-    # Count how many times do a full rotation
+    # Count how many times we do a full rotation
     passes = abs(interimVal // 100)
-    # If we start at 0 going left we double count
+    # If we start at 0 going left we double count our initial position as a click
+    # so we have to decrement
     if cur == 0 and line[0] == 'L':
         passes -= 1
     cur = interimVal % 100
     total += passes
-    # If we end at 0 going left we have to add the final location as a 0
+    # If we end at 0 going left we don't count our last position as a click 
+    # so we have to increment
     if cur == 0 and line[0] == 'L':
         total += 1
 
