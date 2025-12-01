e6119fe32df417de740c584a6ce4835d5e056a80
Garikai <gijimaga@grinnell.edu>
2025-12-01 14:35:54 -0600

    Day 1

---
+++ Day1/Day1.py
@@ -0,0 +1,34 @@
+file_path = "Advent-Of-Code-25/Day1/input.txt"
+
+with open(file_path, "r") as f:
+    lines = f.readlines()
+
+total = 0
+cur = 50
+for line in lines:
+    direction = 1 if line[0] == 'R' else -1
+    turns = int(line[1:])
+    cur = (cur + (direction*turns)) % 100
+    if cur == 0:
+        total += 1
+print(f"First Problem: {total}")
+
+total = 0
+cur = 50
+for line in lines:
+    direction = 1 if line[0] == 'R' else -1
+    turns = int(line[1:])
+    interimVal = cur + (direction*turns)
+    # Count how many times do a full rotation
+    passes = abs(interimVal // 100)
+    # If we start at 0 going left we double count
+    if cur == 0 and line[0] == 'L':
+        passes -= 1
+    cur = interimVal % 100
+    total += passes
+    # If we end at 0 going left we have to add the final location as a 0
+    if cur == 0 and line[0] == 'L':
+        total += 1
+
+print(f"Second Problem: {total}")
+    
